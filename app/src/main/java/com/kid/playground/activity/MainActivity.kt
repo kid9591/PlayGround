a3ad9015bcbe47adce1ddc81bc3defcb2c14e877
Chi <trinhminhchi@gmail.com>
2021-11-24 15:27:38 +0700

    Phải cancel trước khi schedule heartbeat mới.

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -12,8 +12,8 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        HeartbeatUtils.sendFcmBroadcast(this)
         HeartbeatUtils.cancelRequest(this)
+        HeartbeatUtils.sendFcmBroadcast(this)
         HeartbeatUtils.scheduleHeartbeatRequest(this)
     }
 }
--- app/src/main/java/com/kid/playground/receiver/HeartbeatReceiver.kt
+++ app/src/main/java/com/kid/playground/receiver/HeartbeatReceiver.kt
@@ -3,7 +3,6 @@ package com.kid.playground.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.kid.playground.receiver.HeartbeatUtils.cancelRequest
 import com.kid.playground.receiver.HeartbeatUtils.scheduleHeartbeatRequest
 import com.kid.playground.receiver.HeartbeatUtils.sendFcmBroadcast
@@ -15,11 +14,8 @@ import com.kid.playground.receiver.HeartbeatUtils.sendFcmBroadcast
 class HeartbeatReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
 
-        sendFcmBroadcast(context)
         cancelRequest(context)
+        sendFcmBroadcast(context)
         scheduleHeartbeatRequest(context)
-
-//        Log.d("chi.trinh", "HeartbeatReceiver sent heartbeat request")
-//        HeartbeatFixerUtils.scheduleHeartbeatRequest(context, false)
     }
 }
