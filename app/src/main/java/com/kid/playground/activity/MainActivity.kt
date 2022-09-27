7f14a54e3766f8ff258b8ca97a9d8bc0366f8ae6
Chi <trinhminhchi@gmail.com>
2022-09-27 17:07:23 +0700

    Animate mask show part of image.

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -1,68 +1,107 @@
 package com.kid.playground.activity
 
+import android.animation.ValueAnimator
 import android.graphics.*
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.FragmentActivity
-import java.util.Collections.min
+import com.kid.playground.R
 
 
 //https://stackoverflow.com/questions/15091790/how-to-outline-a-textview
 //https://www.android-examples.com/add-set-bitmapshader-texture-effect-on-textview-text-in-android/
 //https://stackoverflow.com/questions/14791012/android-textured-text
 
-class MainActivity : FragmentActivity() {
+class MainActivity : FragmentActivity(), ValueAnimator.AnimatorUpdateListener {
 
 //    private lateinit var stickerView: StickerView
 //    private lateinit var textSticker: TextSticker
 
+    private var ivWidth: Int = 0
+    private var ivHeight: Int = 0
     private lateinit var imageView: ImageView
+
+    private lateinit var source: Bitmap
+    private lateinit var dest: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.kid.playground.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
 //        val heightScale = 660f / 870
 //        val widthScale = 660f / 1229
 
-        val imageView = findViewById<ImageView>(com.kid.playground.R.id.imageview)
+        imageView = findViewById(R.id.imageview)
+        imageView.viewTreeObserver.addOnGlobalLayoutListener {
+            ivWidth = imageView.measuredWidth
+            ivHeight = imageView.measuredHeight
 
-        val mask = BitmapFactory.decodeResource(resources, com.kid.playground.R.mipmap.image_mask)
-
-        val originalResBitmap =
-            BitmapFactory.decodeResource(
-                resources,
-                com.kid.playground.R.mipmap.original1
+            source = BitmapFactory.decodeResource(resources, R.mipmap.image_mask)
+            val originalResBitmap =
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.mipmap.gaixinh
+                )
+            //center crop original on background
+            val scale = 1f / Math.min(
+                originalResBitmap.width.toFloat() / ivWidth,
+                originalResBitmap.height.toFloat() / ivHeight
+            )
+            dest = Bitmap.createScaledBitmap(
+                originalResBitmap,
+                (originalResBitmap.width * scale).toInt(),
+                (originalResBitmap.height * scale).toInt(),
+                true
             )
 
-        //center crop original on mask
-        val scale = 1f / Math.min(originalResBitmap.width.toFloat()/mask.width, originalResBitmap.height.toFloat()/mask.height )
+//            startAnimateLogo(0f, 1500f)
+        }
 
-        Log.d("chi.trinh","scale: $scale")
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startAnimateLogo(0f, 1500f)
+        }
 
-        val original = Bitmap.createScaledBitmap(
-            originalResBitmap, (originalResBitmap.width * scale).toInt(), (originalResBitmap.height * scale).toInt(), true
-        )
+    }
 
-        if (original != null) {
-            //create empty bitmap with width & height
-            val resultBitmap = Bitmap.createBitmap(mask.width, mask.height, Bitmap.Config.ARGB_8888)
+    private fun draw(yPos: Float) {
 
-            val canvas = Canvas(resultBitmap)
+        //create empty bitmap with width & height
+        val transparentBitmap = Bitmap.createBitmap(ivWidth, ivHeight, Bitmap.Config.ARGB_8888)
 
-            val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
-
-            canvas.drawBitmap(original, (mask.width - original.width)/2f, (mask.height - original.height)/2f, null)
-            canvas.drawBitmap(mask, 0f, 0f, paint)
-
-            paint.xfermode = null
-            paint.style = Paint.Style.STROKE
+        val sourceAdjustCanvas = Canvas(transparentBitmap)
+        sourceAdjustCanvas.drawBitmap(source, (ivWidth - source.width) / 2f, yPos, null)
 
 
-            imageView.setImageBitmap(resultBitmap)
+        val resultBitmap = Bitmap.createBitmap(ivWidth, ivHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(resultBitmap)
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        canvas.drawBitmap(dest, (ivWidth - dest.width) / 2f, (ivHeight - dest.height) / 2f, paint)
+
+        val mode: PorterDuff.Mode = PorterDuff.Mode.DST_IN // choose a mode
+        paint.xfermode = PorterDuffXfermode(mode)
+
+        canvas.drawBitmap(transparentBitmap, 0f, 0f, paint)
+
+        imageView.setImageBitmap(resultBitmap)
+    }
+
+    private fun startAnimateLogo(fromY: Float, toY: Float) {
+        ValueAnimator.ofFloat(fromY, toY).apply {
+            duration = 2500
+            addUpdateListener(this@MainActivity)
+//            addListener(this@MainActivity)
+            start()
         }
     }
+
+    override fun onAnimationUpdate(animator: ValueAnimator) {
+//        logoAnimatingAngle = animator.animatedValue as? Float ?: -1f
+//        invalidate()
+
+        draw(animator.animatedValue as? Float ?: 0f)
+    }
+
 }
 
