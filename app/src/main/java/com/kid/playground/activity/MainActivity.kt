1cf8afc1a58b96c59fac4ceb1b1ddea2555a9d23
Chi <trinhminhchi@gmail.com>
2022-10-12 17:18:51 +0700

    Demo OpenGL Es 2.0

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -1,107 +1,202 @@
 package com.kid.playground.activity
 
-import android.animation.ValueAnimator
-import android.graphics.*
+import android.content.Context
+import android.opengl.EGLConfig
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import com.kid.playground.R
+import org.bouncycastle.pqc.math.linearalgebra.IntegerFunctions.order
+import org.slf4j.MDC.put
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+import javax.microedition.khronos.opengles.GL10
 
 
 //https://stackoverflow.com/questions/15091790/how-to-outline-a-textview
 //https://www.android-examples.com/add-set-bitmapshader-texture-effect-on-textview-text-in-android/
 //https://stackoverflow.com/questions/14791012/android-textured-text
 
-class MainActivity : FragmentActivity(), ValueAnimator.AnimatorUpdateListener {
+class MainActivity : FragmentActivity()  {
+
+
+
+
 
 //    private lateinit var stickerView: StickerView
 //    private lateinit var textSticker: TextSticker
 
-    private var ivWidth: Int = 0
-    private var ivHeight: Int = 0
-    private lateinit var imageView: ImageView
-
-    private lateinit var source: Bitmap
-    private lateinit var dest: Bitmap
+    private lateinit var gLView: GLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//        val heightScale = 660f / 870
-//        val widthScale = 660f / 1229
-
-        imageView = findViewById(R.id.imageview)
-        imageView.viewTreeObserver.addOnGlobalLayoutListener {
-            ivWidth = imageView.measuredWidth
-            ivHeight = imageView.measuredHeight
-
-            source = BitmapFactory.decodeResource(resources, R.mipmap.image_mask)
-            val originalResBitmap =
-                BitmapFactory.decodeResource(
-                    resources,
-                    R.mipmap.gaixinh
-                )
-            //center crop original on background
-            val scale = 1f / Math.min(
-                originalResBitmap.width.toFloat() / ivWidth,
-                originalResBitmap.height.toFloat() / ivHeight
-            )
-            dest = Bitmap.createScaledBitmap(
-                originalResBitmap,
-                (originalResBitmap.width * scale).toInt(),
-                (originalResBitmap.height * scale).toInt(),
-                true
-            )
-
-//            startAnimateLogo(0f, 1500f)
-        }
-
-        findViewById<Button>(R.id.button).setOnClickListener {
-            startAnimateLogo(0f, 1500f)
-        }
-
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
     }
-
-    private fun draw(yPos: Float) {
-
-        //create empty bitmap with width & height
-        val transparentBitmap = Bitmap.createBitmap(ivWidth, ivHeight, Bitmap.Config.ARGB_8888)
-
-        val sourceAdjustCanvas = Canvas(transparentBitmap)
-        sourceAdjustCanvas.drawBitmap(source, (ivWidth - source.width) / 2f, yPos, null)
-
-
-        val resultBitmap = Bitmap.createBitmap(ivWidth, ivHeight, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(resultBitmap)
-
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        canvas.drawBitmap(dest, (ivWidth - dest.width) / 2f, (ivHeight - dest.height) / 2f, paint)
-
-        val mode: PorterDuff.Mode = PorterDuff.Mode.DST_IN // choose a mode
-        paint.xfermode = PorterDuffXfermode(mode)
-
-        canvas.drawBitmap(transparentBitmap, 0f, 0f, paint)
-
-        imageView.setImageBitmap(resultBitmap)
-    }
-
-    private fun startAnimateLogo(fromY: Float, toY: Float) {
-        ValueAnimator.ofFloat(fromY, toY).apply {
-            duration = 2500
-            addUpdateListener(this@MainActivity)
-//            addListener(this@MainActivity)
-            start()
-        }
-    }
-
-    override fun onAnimationUpdate(animator: ValueAnimator) {
-//        logoAnimatingAngle = animator.animatedValue as? Float ?: -1f
-//        invalidate()
-
-        draw(animator.animatedValue as? Float ?: 0f)
-    }
-
 }
+
+class Triangle {
+
+    companion object {
+        // number of coordinates per vertex in this array
+        const val COORDS_PER_VERTEX = 3
+    }
+
+    fun loadShader(type: Int, shaderCode: String): Int {
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        return GLES20.glCreateShader(type).also { shader ->
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode)
+            GLES20.glCompileShader(shader)
+        }
+    }
+
+    private val vertexShaderCode =
+        "attribute vec4 vPosition;" +
+                "void main() {" +
+                "  gl_Position = vPosition;" +
+                "}"
+
+    private val fragmentShaderCode =
+        "precision mediump float;" +
+                "uniform vec4 vColor;" +
+                "void main() {" +
+                "  gl_FragColor = vColor;" +
+                "}"
+
+    var triangleCoords = floatArrayOf(     // in counterclockwise order:
+        -1f, -1f, 0.0f,
+//        -1f, 1f, 0.0f,
+//        1f, -1f, 0.0f,
+//        -1f, 1f, 0.0f,
+    )
+
+    private var mProgram: Int
+
+    init {
+        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram().also {
+
+            // add the vertex shader to program
+            GLES20.glAttachShader(it, vertexShader)
+
+            // add the fragment shader to program
+            GLES20.glAttachShader(it, fragmentShader)
+
+            // creates OpenGL ES program executables
+            GLES20.glLinkProgram(it)
+        }
+    }
+
+    private var positionHandle: Int = 0
+    private var mColorHandle: Int = 0
+
+    private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
+    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
+
+    fun draw() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram)
+
+        // get handle to vertex shader's vPosition member
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
+
+            // Enable a handle to the triangle vertices
+            GLES20.glEnableVertexAttribArray(it)
+
+            // Prepare the triangle coordinate data
+            GLES20.glVertexAttribPointer(
+                it,
+                COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT,
+                false,
+                vertexStride,
+                vertexBuffer
+            )
+
+            // get handle to fragment shader's vColor member
+            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
+
+                // Set color for drawing the triangle
+                GLES20.glUniform4fv(colorHandle, 1, color, 0)
+            }
+
+            // Draw the triangle
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
+
+            // Disable vertex array
+            GLES20.glDisableVertexAttribArray(it)
+        }
+    }
+
+    // Set color with red, green, blue and alpha (opacity) values
+    val color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
+
+    private var vertexBuffer: FloatBuffer =
+        // (number of coordinate values * 4 bytes per float)
+        ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
+            // use the device hardware's native byte order
+            order(ByteOrder.nativeOrder())
+
+            // create a floating point buffer from the ByteBuffer
+            asFloatBuffer().apply {
+                // add the coordinates to the FloatBuffer
+                put(triangleCoords)
+                // set the buffer to read the first coordinate
+                position(0)
+            }
+        }
+}
+
+class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+
+    private val renderer: MyGLRenderer
+
+    init {
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2)
+
+        renderer = MyGLRenderer()
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer)
+        renderMode = RENDERMODE_WHEN_DIRTY
+    }
+}
+
+class MyGLRenderer : GLSurfaceView.Renderer {
+
+    private lateinit var mTriangle: Triangle
+
+    override fun onDrawFrame(unused: GL10) {
+        // Redraw background color
+//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        Log.d("chi.trinh","onDrawFrame")
+        mTriangle.draw()
+    }
+
+    override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
+        // Set the background frame color
+//        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        Log.d("chi.trinh","onSurfaceCreated")
+
+        mTriangle = Triangle()
+    }
+
+    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
+        Log.d("chi.trinh","onSurfaceChanged")
+        GLES20.glViewport(0, 0, width, height)
+    }
+}
+
 
