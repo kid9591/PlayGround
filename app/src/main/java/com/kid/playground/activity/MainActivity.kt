a9948a6b878e55ca4bc92af13d2d6e9f2ab2e721
Chi <trinhminhchi@gmail.com>
2022-10-13 17:09:58 +0700

    Parse CSV

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.kid.playground.activity
 
-import android.content.Context
-import android.opengl.EGLConfig
-import android.opengl.GLES20
-import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.fragment.app.FragmentActivity
 import com.kid.playground.R
-import org.bouncycastle.pqc.math.linearalgebra.IntegerFunctions.order
-import org.slf4j.MDC.put
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.FloatBuffer
-import javax.microedition.khronos.opengles.GL10
+import com.opencsv.CSVReader
+import java.io.FileReader
 
 
 //https://stackoverflow.com/questions/15091790/how-to-outline-a-textview
@@ -23,179 +16,26 @@ import javax.microedition.khronos.opengles.GL10
 class MainActivity : FragmentActivity()  {
 
 
-
-
-
-//    private lateinit var stickerView: StickerView
-//    private lateinit var textSticker: TextSticker
-
-    private lateinit var gLView: GLSurfaceView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        gLView = MyGLSurfaceView(this)
-        setContentView(gLView)
-    }
-}
+        setContentView(R.layout.activity_main)
 
-class Triangle {
+        val path = "$cacheDir/export_barcode_scan.csv"
 
-    companion object {
-        // number of coordinates per vertex in this array
-        const val COORDS_PER_VERTEX = 3
-    }
-
-    fun loadShader(type: Int, shaderCode: String): Int {
-
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-        return GLES20.glCreateShader(type).also { shader ->
-
-            // add the source code to the shader and compile it
-            GLES20.glShaderSource(shader, shaderCode)
-            GLES20.glCompileShader(shader)
-        }
-    }
-
-    private val vertexShaderCode =
-        "attribute vec4 vPosition;" +
-                "void main() {" +
-                "  gl_Position = vPosition;" +
-                "}"
-
-    private val fragmentShaderCode =
-        "precision mediump float;" +
-                "uniform vec4 vColor;" +
-                "void main() {" +
-                "  gl_FragColor = vColor;" +
-                "}"
-
-    var triangleCoords = floatArrayOf(     // in counterclockwise order:
-        -1f, -1f, 0.0f,
-//        -1f, 1f, 0.0f,
-//        1f, -1f, 0.0f,
-//        -1f, 1f, 0.0f,
-    )
-
-    private var mProgram: Int
-
-    init {
-        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
-        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
-
-        // create empty OpenGL ES Program
-        mProgram = GLES20.glCreateProgram().also {
-
-            // add the vertex shader to program
-            GLES20.glAttachShader(it, vertexShader)
-
-            // add the fragment shader to program
-            GLES20.glAttachShader(it, fragmentShader)
-
-            // creates OpenGL ES program executables
-            GLES20.glLinkProgram(it)
-        }
-    }
-
-    private var positionHandle: Int = 0
-    private var mColorHandle: Int = 0
-
-    private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
-    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
-
-    fun draw() {
-        // Add program to OpenGL ES environment
-        GLES20.glUseProgram(mProgram)
-
-        // get handle to vertex shader's vPosition member
-        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition").also {
-
-            // Enable a handle to the triangle vertices
-            GLES20.glEnableVertexAttribArray(it)
-
-            // Prepare the triangle coordinate data
-            GLES20.glVertexAttribPointer(
-                it,
-                COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT,
-                false,
-                vertexStride,
-                vertexBuffer
-            )
-
-            // get handle to fragment shader's vColor member
-            mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor").also { colorHandle ->
-
-                // Set color for drawing the triangle
-                GLES20.glUniform4fv(colorHandle, 1, color, 0)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val reader = CSVReader(FileReader(path), ';')
+            val myEntries: List<*> = reader.readAll()
+            val pairs = mutableListOf<Pair<String, String>>()
+            myEntries.forEach { row ->
+                (row as? Array<*>)?.let { columns ->
+                    pairs.add(Pair(columns.first().toString(), columns.last().toString()))
+                }
             }
-
-            // Draw the triangle
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
-
-            // Disable vertex array
-            GLES20.glDisableVertexAttribArray(it)
-        }
-    }
-
-    // Set color with red, green, blue and alpha (opacity) values
-    val color = floatArrayOf(0.63671875f, 0.76953125f, 0.22265625f, 1.0f)
-
-    private var vertexBuffer: FloatBuffer =
-        // (number of coordinate values * 4 bytes per float)
-        ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
-            // use the device hardware's native byte order
-            order(ByteOrder.nativeOrder())
-
-            // create a floating point buffer from the ByteBuffer
-            asFloatBuffer().apply {
-                // add the coordinates to the FloatBuffer
-                put(triangleCoords)
-                // set the buffer to read the first coordinate
-                position(0)
+            pairs.forEachIndexed { index, it ->
+                Log.d("chi.trinh","#$index ${it.first}:${it.second}")
             }
         }
-}
-
-class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
-
-    private val renderer: MyGLRenderer
-
-    init {
-
-        // Create an OpenGL ES 2.0 context
-        setEGLContextClientVersion(2)
-
-        renderer = MyGLRenderer()
-        // Set the Renderer for drawing on the GLSurfaceView
-        setRenderer(renderer)
-        renderMode = RENDERMODE_WHEN_DIRTY
-    }
-}
-
-class MyGLRenderer : GLSurfaceView.Renderer {
-
-    private lateinit var mTriangle: Triangle
-
-    override fun onDrawFrame(unused: GL10) {
-        // Redraw background color
-//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        Log.d("chi.trinh","onDrawFrame")
-        mTriangle.draw()
-    }
-
-    override fun onSurfaceCreated(gl: GL10?, config: javax.microedition.khronos.egl.EGLConfig?) {
-        // Set the background frame color
-//        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-        Log.d("chi.trinh","onSurfaceCreated")
-
-        mTriangle = Triangle()
-    }
-
-    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
-        Log.d("chi.trinh","onSurfaceChanged")
-        GLES20.glViewport(0, 0, width, height)
     }
 }
 
