4fc8f368beeb1346cef463cb1715a4e6b935a75a
Chi <trinhminhchi@gmail.com>
2024-02-22 01:08:19 +0700

    Print log with stacktrace throwable (clickable to go function)

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.kid.playground.activity
 
 import android.app.Activity
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.Priority
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.kid.playground.R
-import org.apache.commons.lang3.exception.ExceptionUtils
-import java.util.Random
-import kotlin.math.sign
+import java.util.logging.Logger
+import java.util.logging.Logger.getLogger
 
 
 class MainActivity : Activity() {
@@ -40,11 +27,12 @@ class MainActivity : Activity() {
     }
 
     private fun fun3() {
-        Thread.currentThread().stackTrace.forEach {
-            if (it.className.contains(this::class.java.simpleName)) {
-                Log.d("chi.trinh", "${it.className}.${it.methodName}()")
-            }
-        }
+//        Thread.currentThread().stackTrace.forEach {
+//            if (it.className.contains(this::class.java.simpleName)) {
+//                Log.d("chi.trinh", "${it.className}.${it.methodName}()")
+//            }
+//        }
+        Log.d("chi.trinh","Callstack to this function",Throwable())
     }
 
 }
