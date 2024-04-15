6420b475cf011185aead444a82eb2135ea0dfd4b
Chi <trinhminhchi@gmail.com>
2024-04-15 15:29:06 +0700

    Expandable content webview Fix case collapse content is higher than original one. (show full & no "More" button)

--- app/src/main/java/com/kid/playground/activity/ExpandableContentWebView.kt
+++ app/src/main/java/com/kid/playground/activity/ExpandableContentWebView.kt
@@ -1,13 +1,21 @@
 package com.kid.playground.activity
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.ViewTreeObserver.OnPreDrawListener
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.core.view.isVisible
 import com.kid.playground.R
+
 
 /**
  * @author kienht
@@ -28,7 +36,7 @@ class ExpandableContentWebView @JvmOverloads constructor(
 
         // Retrieve custom attribute value from XML
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableContentWebView)
-        val htmlText = typedArray.getString(R.styleable.ExpandableContentWebView_html)
+        val htmlText = "<p>Ruhig gelegene Pension, Zimmer ebenerdig, Haustiere willkommen, Familienanschluss, Fahrradweg vor der Haustür<br></p>"//typedArray.getString(R.styleable.ExpandableContentWebView_html)
         typedArray.recycle()
 
         val relativeShortDesc = findViewById<RelativeLayout>(R.id.relative_short_desc)
@@ -49,5 +57,33 @@ class ExpandableContentWebView @JvmOverloads constructor(
             textDesc.isVisible = isDescExpanded
             relativeShortDesc.isVisible = !isDescExpanded
         }
+
+        textDesc.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                view?.loadUrl("javascript:AndroidFunction.resize(document.body.scrollHeight)");
+            }
+        }
+        textDesc.addJavascriptInterface(object {
+            @JavascriptInterface
+            fun resize(height: Float) {
+
+                Handler(Looper.getMainLooper()).post {
+                    val fullHeight = (height * context.resources.displayMetrics.density).toInt()
+                    val shortHeight = relativeShortDesc.height
+
+                    Log.d("chi.trinh", "ExpandableContentWebView >> fullHeight : $fullHeight")
+                    Log.d("chi.trinh", "ExpandableContentWebView >>  shortHeight : $shortHeight")
+                    if (shortHeight < fullHeight) {
+                        relativeShortDesc.isVisible = true
+                        textDesc.isVisible = false
+                    } else {
+                        relativeShortDesc.isVisible = false
+                        textDesc.isVisible = true
+                        button.isVisible = false
+                    }
+                }
+            }
+        }, "AndroidFunction")
+
     }
 }
