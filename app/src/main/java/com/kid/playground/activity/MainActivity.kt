dd8e0609955bc5e2d65f1a0575f63c9c36e8992f
Chi <trinhminhchi@gmail.com>
2024-08-12 14:16:04 +0700

    Item color for BottomNavigationBar

--- app/src/main/java/com/kid/playground/activity/MainActivity.kt
+++ app/src/main/java/com/kid/playground/activity/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.kid.playground.activity
 
-import android.content.Context
-import android.graphics.Typeface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.StateListDrawable
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.kid.playground.R
 import com.kid.playground.databinding.ActivityMainBinding
-import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,28 +21,19 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
             this.viewmodel = this@MainActivity.viewmodel
             this.lifecycleOwner = this@MainActivity
-
-            button.setOnClickListener {
-                val fontFile = File(filesDir, "impact.ttf")
-                val typeface = Typeface.createFromFile(fontFile)
-                this@MainActivity.viewmodel.setTypeface(typeface)
-                this@MainActivity.viewmodel.setFontSize(140)
-                this@MainActivity.viewmodel.setText("abc")
-            }
+            bottomNavigation.itemBackground = StateListDrawable()
+                .apply {
+                    setExitFadeDuration(200)
+//                    alpha = 45
+                    addState(
+                        intArrayOf(android.R.attr.state_checked),
+                        ColorDrawable(Color.RED)
+                    )
+                    addState(
+                        intArrayOf(),
+                        ColorDrawable(Color.BLUE)
+                    )
+                }
         }
-    }
-}
-
-class ColorableDropDownAdapter(
-    private val textColor: Int,
-    context: Context,
-    itemRes: Int,
-    items: List<String>
-) : ArrayAdapter<String>(context, itemRes, items) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = super.getView(position, convertView, parent)
-        val textview = view.findViewById<TextView>(android.R.id.text1)
-        textview.setTextColor(textColor)
-        return view
     }
 }
