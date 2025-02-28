2eae5a546226815cd7b5f8309724f7784b51b595
Chi <trinhminhchi@gmail.com>
2025-02-28 17:03:47 +0700

    Deeplink: Click this link will open app. Need to add assetlinks.json to https://blupassionsystem.de/.well-known/assetlinks.json https://blupassionsystem.de/b2bwebapp/#/57290/index/formsubmitteddetail/9 ->

--- app/src/main/java/com/kid/playground/activity/NewActivity.kt
+++ app/src/main/java/com/kid/playground/activity/NewActivity.kt
@@ -3,8 +3,10 @@ package com.kid.playground.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.kid.playground.R
+import android.net.Uri
 
 
 //https://stackoverflow.com/questions/15091790/how-to-outline-a-textview
@@ -16,12 +18,20 @@ class NewActivity : FragmentActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-        Log.d("chi.trinh", "data: ${intent.data}")
         //Some changes here
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
+
+        Log.d("chi.trinh", "appLinkAction : $appLinkAction")
+        Log.d("chi.trinh", "appLinkData : ${appLinkData.toString()}")
+
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
+        Toast.makeText(this, "New Activity new intent", Toast.LENGTH_LONG).show()
         Log.d("chi.trinh", "new intent data: ${intent!!.data}")
     }
 
